8f70b16dc93c33f6adaa242f466996852b6c1ee1
unknown <luis.daniel.tang@gmail.com>
2023-05-26 20:45:58 -0300

    Api Paginacion Actualizacion

--- src/app/movies-list/movies-list.page.ts
+++ src/app/movies-list/movies-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.page.html',
@@ -8,18 +9,29 @@ import {HttpClient} from '@angular/common/http';
 export class MoviesListPage implements OnInit {
 
   movies!:any[]
+  page:number=1;
 
   constructor(
-    private http:HttpClient
+    private http:HttpClient,
+    private router: Router
   ) { }
 
   createTemplate(url:string){
     const template=`https://image.tmdb.org/t/p/w500/${url}`
     return template
   }
-
+  nextPage(){
+    this.page++;
+    console.log(this.page);
+    this.router.navigate(['movies-list']);
+  }
+  backPage(){
+    this.page--;
+    console.log(this.page);
+    this.router.navigate(['/movies-list']);
+  }
   ngOnInit() {
-    this.http.get<any>('https://api.themoviedb.org/3/discover/movie?api_key=373d3d64f286e9fc62879bb68579a452&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+    this.http.get<any>(`https://api.themoviedb.org/3/discover/movie?api_key=373d3d64f286e9fc62879bb68579a452&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=${String(this.page)}&with_watch_monetization_types=flatrate`)
     .subscribe(res=>{
       console.log(res);
       this.movies=res.results;
