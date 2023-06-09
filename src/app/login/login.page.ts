b9ed0dbd5c40fb592f4d4b27aa5b19b2f61a6e41
unknown <luis.daniel.tang@gmail.com>
2023-06-09 20:50:14 -0300

    loginAndRegisterSuccessful

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../services/auth.service";
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public authService: AuthenticationService,
+    public router: Router
+  ) {}
+  ngOnInit() {}
+  logIn(f:NgForm) : void {
+    this.authService.SignIn(f.value.email, f.value.password)
+      .then((res) => {
+        if(this.authService.isEmailVerified) {
+          this.router.navigate(['movies-list']);          
+        } else {
+          window.alert('Email is not verified')
+          return false;
+        }
+      }).catch((error) => {
+        window.alert(error.message)
+        return
+      })
   }
-
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,32 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../services/auth.service";
 import {NgForm} from '@angular/forms';
-//import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { 
-
+  constructor(
+    public authService: AuthenticationService,
+    public router: Router
+  ) { }
+  ngOnInit(){}
+  signUp(f:NgForm){
+      this.authService.RegisterUser(f.value.email, f.value.password)      
+      .then((res) => {
+        alert('usuario registrado')
+        this.router.navigate(['login']);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
   }
-
-  ngOnInit() {
-  }
-
-  async onRegister(f:NgForm) {
-    try {
-    const data=f.value; //email,password
-    // const user= await this.authSvc.register(data.email,data.password);
-    // if(user){
-    //   console.log(user);
-    // }
-    console.log(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  
-
 }
